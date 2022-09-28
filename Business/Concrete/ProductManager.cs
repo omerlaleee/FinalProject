028a74ad2cc149ca22fab33f3001419fa1589d98
Ömer Faruk LALE <omerlaleee@gmail.com>
2022-09-28 15:18:23 +0300

    Cache Structure was added to the project.

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -2,6 +2,7 @@
 using Business.BusinessAspects.Autofac;
 using Business.Constants;
 using Business.ValidationRules.FluentValidation;
+using Core.Aspects.Autofac.Caching;
 using Core.Aspects.Autofac.Validation;
 using Core.CrossCuttingConcerns.Validation;
 using Core.Utilities.Business;
@@ -32,6 +33,7 @@ namespace Business.Concrete
 
         [SecuredOperation("admin,product.add")] // You have to give the claims without any whitespace before or after comma.
         [ValidationAspect(typeof(ProductValidator))]
+        [CacheRemoveAspect("IProductService.Get")]
         public IResult Add(Product product)
         {
             var ruleResult = BusinessRules.Run(CheckIfProductCountOfCategoryCorrect(product.CategoryId),
@@ -52,12 +54,14 @@ namespace Business.Concrete
             return new SuccessResult(Messages.ProductDeleted);
         }
 
+        [CacheAspect] // → Key / Value Pair
         public IDataResult<Product> GetById(int productId)
         {
             return new DataResult<Product>(true, "Ürün Listelendi", _productDal.Get(p => p.ProductId == productId));
             //return _productDal.Get(p => p.ProductId == productId);
         }
 
+        [CacheAspect]
         public IDataResult<List<Product>> GetAll()
         {
             //if (DateTime.Now.Hour == 14)
@@ -68,6 +72,7 @@ namespace Business.Concrete
         }
 
         [ValidationAspect(typeof(ProductValidator))]
+        [CacheRemoveAspect("IProductService.Get")]
         public IResult Update(Product product)
         {
             var ruleResult = BusinessRules.Run(CheckIfProductCountOfCategoryCorrect(product.CategoryId),
